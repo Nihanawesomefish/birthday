0a90fb131168680a67b09d07bea8e95cccc89baf
Nihanawesomefish <80280683+Nihanawesomefish@users.noreply.github.com>
2021-10-23 12:58:02 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL("BirthdayImage.jpg",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(750);
+        player_object.scaleToHeight(500);
+        player_object.set({
+        top:0,
+        left:0
+        });
+        canvas.add(player_object);
+        });
+            
+	
+}
+
+function playSound(){
+new_image();
+document.getElementById("myAudio").play();
+}
